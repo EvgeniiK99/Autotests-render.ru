4ccb3120d3afa2fd793aa9e2560a00f5d5eedbb6
Evgenii Klimashin <evgeniiklimashin99@gmail.com>
2023-08-12 17:36:37 +0300

    fix

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -18,7 +18,7 @@ public class TestBase {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+//        Configuration.holdBrowserOpen = true;
         Configuration.remote ="https://user1:1234@selenoid.autotests.cloud/wd/hub";
 //        Configuration.timeout = 30000;
     }
